1c68004840b56f57a7a735b399c9cb5b37cd7147
Manuel Araoz <manuelaraoz@gmail.com>
2018-05-26 12:13:11 -0300

    initial commit

---
+++ truffle-config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  networks: {
+    local: {
+      host: 'localhost',
+      port: 9545,
+      gas: 5000000,
+      network_id: '*'
+    },
+    seba: {
+      host: '0.tcp.ngrok.io',
+      port: 19575,
+      network_id: '1'
+    },
+    zep: {
+      host: 'localhost',
+      port: 8546,
+      network_id: '1'
+    }
+  }
+};
