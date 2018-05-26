c89585cd6b0110ea896acbb1a04802591d56b48c
Agustin Esteban Aguilar <agustin@bitpagos.co>
2018-05-26 18:37:08 -0300

    CatDestiny

--- contracts/interfaces/Destiny.sol
+++ contracts/interfaces/Destiny.sol
@@ -1,5 +1,5 @@
 pragma solidity 0.4.24;
 
 contract Destiny {
-    function fight(bytes32 cat1, bytes32 cat2, bytes32 entropy) public view returns (bytes32 winner);
+    function fight(bytes32 cat1, bytes32 cat2, bytes32 entropy) public returns (bytes32 winner);
 }
