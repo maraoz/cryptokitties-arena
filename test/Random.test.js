9f8fb5028f61f995c4a376357181b2ac498dcba6
Manuel Araoz <manuelaraoz@gmail.com>
2018-05-26 13:07:44 -0300

    add PRNG

---
+++ test/Random.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Random = artifacts.require('Random');
+const RandomUseExample = artifacts.require('RandomUseExample');
+
+contract('Random', function () {
+  beforeEach(async function () {
+    this.random = await Random.new();
+    this.dice = await RandomUseExample.new(this.random.address);
+  });
+
+  it('can generate random numbers', async function() {
+    await this.dice.roll()
+    var d6 = await this.dice.d6()
+    console.log(d6)
+    d6 = await this.dice.d6()
+    console.log(d6)
+    d6 = await this.dice.d6()
+    console.log(d6)
+    d6 = await this.dice.d6()
+    console.log(d6)
+  })
+
+});
