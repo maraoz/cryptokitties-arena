fdf52cc8eb32cf4546368b8beb26f3e3181992b3
Sebastian Serrano <sebastian@ripio.com>
2018-05-27 04:18:00 -0300

    call contracts

--- app/src/index.js
+++ app/src/index.js
@@ -10,6 +10,11 @@ import logo from './assets/img/logo-crypto-kitties-fight.svg';
 import './index.css';
 import { debug } from 'util';
 
+var KittyArenaABI = require("./assets/contracts/KittyArena.json");
+var KittyArenaAddress = "0x3686e7Ec62D5694A8aC05f8F39FD5F632e2fF79b";
+var KittyCoreABI = require("./assets/contracts/KittyCore.json");
+var KittyCoreAddress = "0x06012c8cf97BEaD5deAe237070F9587f8E7A266d";
+
 var Web3 = require('web3');
 var request = require('request');
 
@@ -33,8 +38,33 @@ window.addEventListener('load', function() {
 })
 
 class Kitty extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'kitty': props.kitty
+    }
+    this.authorize = this.authorize.bind(this);
+    this.enterArena = this.enterArena.bind(this);
+  }
+
+  authorize(e) {
+    e.preventDefault();
+    this.props.KittyCore.approve(KittyArenaAddress, this.state.kitty.id, {from: this.props.account})
+  }
+
+  enterArena(e) {
+    e.preventDefault();
+    this.props.KittyArena.enter(this.state.kitty.id, {from: this.props.account})
+  }
+
   render() {
-    return (<p>Kitty</p>)
+    return (
+      <span>
+        <img width="100px" src={this.state.kitty.image_url_cdn}/>
+        <button className="btn" onClick={this.authorize}>Authorize</button>
+        <button className="btn" onClick={this.enterArena}>Enter Arena</button>
+      </span>
+    )
   }
 }
 
@@ -42,8 +72,7 @@ function KittiesList(props) {
   const kitties = props.kitties || [];
   const listItems = kitties.map((kitty) =>
     <li key={kitty.id}>
-      <img width="100px" src={kitty.image_url_cdn}/>
-      {kitty.name}
+      <Kitty kitty={kitty} KittyCore={props.KittyCore} account={props.account} KittyArena={props.KittyArena}/>
     </li>
   );
   return (
@@ -58,16 +87,25 @@ class Kitties extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      'kitties': [{'id': 1}],
+      'kitties': [],
       'loading': true,
+      'KittyCore': null,
+      'account': null
     }
     var that = this;
     window.addEventListener('load', function() {
       if (typeof web3 !== 'undefined') {
         var myWeb3 = new Web3(window.web3.currentProvider);
+
+        var KittyArena = window.web3.eth.contract(KittyArenaABI).at(KittyArenaAddress);
+        that.state.KittyArena = KittyArena;
+        var KittyCore = window.web3.eth.contract(KittyCoreABI).at(KittyCoreAddress);
+        that.state.KittyCore = KittyCore;
+
         myWeb3.eth.getAccounts(function (err, accounts) {
+          that.setState({'account': accounts[0]});
           getKitties(accounts[0], function(kitties) {
-            console.log(kitties)
+            console.log(kitties);
             that.setState({'kitties': kitties, 'loading': false});
           });
         });
@@ -75,7 +113,10 @@ class Kitties extends React.Component {
     });
   }
   render() {
-    return (<KittiesList kitties={this.state.kitties} />)
+    return (<KittiesList kitties={this.state.kitties} 
+                         KittyCore={this.state.KittyCore} 
+                         KittyArena={this.state.KittyArena}
+                         account={this.state.account}/>)
   }
 }
 
